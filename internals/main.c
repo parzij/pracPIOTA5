d197229b1df0ac2b980c709f7fb4675b63409772
parzi <batychkov@sfedu.ru>
2025-05-11 23:04:48 +0300

    color && rus encoding

--- internals/main.c
+++ internals/main.c
@@ -1,48 +1,60 @@
 #include "TuringMachine.h"
+#include <windows.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
 #define cnMAX   256
-#define maxStep 10000
+#define maxStep 10000000
 
 /*
  Подпрограмма проверяет, является ли строка двоичным числом
- Вход : указатель на строку
+ Вход :
+    1) указатель на строку
  Выход: true, если непуста и состоит только из '0' и '1'
 */
 bool isBinary(char *str);
 
 int main(void) {
-    TM_Machine tm;
-    bool initOk = tmInit(&tm, "tm_table.txt", '_');
-    bool isWork = initOk;
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD  mode;
+    if (GetConsoleMode(hOut, &mode))
+        SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 
-    if (!initOk) 
+    TM_Machine tm;
+    bool initOk = tmInit(&tm, "tmTable.txt", '_');
+    bool isWork   = initOk;
+
+    if (!initOk)
         printf("Ошибка чтения файла системы команд.\n");
 
     while (isWork) {
-        char arrBuf[cnMAX];
+        char arrBuff[cnMAX];
         bool isCorrect = false;
+
         while (!isCorrect) {
             printf("Введите неотрицательное число в двоичной форме (или \"exit\"): ");
-            if (scanf("%255s", arrBuf) != 1) {
+            if (scanf("%255s", arrBuff) != 1) {
                 isWork = false;
                 isCorrect = true;
-            } else if (strcmp(arrBuf, "exit") == 0) {
+            } else if (strcmp(arrBuff, "exit") == 0) {
                 isWork = false;
                 isCorrect = true;
-            } else if (isBinary(arrBuf))
+            } else if (isBinary(arrBuff))
                 isCorrect = true;
             else
                 printf("Ошибка: допустимы только символы 0 и 1.\n");
         }
+
         if (isWork) {
-            tmLoadTape(&tm, arrBuf);
+            tmLoadTape(&tm, arrBuff);
             tmRun(&tm, maxStep);
         }
     }
-    tmFree(&tm); 
+
+    tmFree(&tm);
     return 0;
 }
 
